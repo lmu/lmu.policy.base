8c3505bff4f519346846b5dc1376434d833426a0
Manuel Reinhardt <reinhardt@syslab.com>
2015-02-26 16:57:26 +0000

    get extra types from site_properties

--- lmu/policy/base/browser.py
+++ lmu/policy/base/browser.py
@@ -1,16 +1,23 @@
+from Products.CMFCore.utils import getToolByName
 from plone.app.search.browser import Search as BaseSearch
 
 
 class Search(BaseSearch):
+
+    def extra_types(self):
+        ptool = getToolByName(self, 'portal_properties')
+        siteProperties = getattr(ptool, 'site_properties')
+        extraTypes = siteProperties.getProperty('extra_types_searched', [])
+        return list(extraTypes)
 
     def filter_query(self, query):
         query = super(Search, self).filter_query(query)
         if query:
             if 'path' in query:
                 del query['path']
-            query['portal_type'] += ["zuv_person"]
+            query['portal_type'] += self.extra_types()
         return query
 
     def types_list(self):
         types = super(Search, self).types_list()
-        return types + ["zuv_person"]
+        return types + self.extra_types()
