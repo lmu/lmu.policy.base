20e8f4f70d689ebc06f066521ef2bbbd1ba6d815
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-05-05 10:44:30 +0200

    fix if effective is ot set via Plone, so if created is larger than effective created is shown if EffectiveDate is called on a SolrFlare

--- lmu/policy/base/patches/collective_solr_flare_PloneFlare_EffectiveDate_patch.py
+++ lmu/policy/base/patches/collective_solr_flare_PloneFlare_EffectiveDate_patch.py
@@ -13,6 +13,9 @@ def EffectiveDate(self):
     effective = self.get('effective', None)
     if effective is None:
         return 'n.a.'
+    created = self.get('created', None)
+    if created is not None and effective < created:
+        return created.toZone(timezone).ISO8601()
     return effective.toZone(timezone).ISO8601()
 
 
