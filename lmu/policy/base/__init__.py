ce12378f8e9395f239a3c937d8e2f743a8fecede
Manuel Reinhardt <reinhardt@syslab.com>
2015-05-26 14:34:06 +0100

    added breadcrumb overrides

--- lmu/policy/base/__init__.py
+++ lmu/policy/base/__init__.py
@@ -1,1 +1,6 @@
+from zope.i18nmessageid import MessageFactory
 import patches
+
+patches  # flake8
+
+LMUMessageFactory = MessageFactory('lmu.poliy.base')
--- lmu/policy/base/browser.py
+++ lmu/policy/base/browser.py
@@ -14,11 +14,15 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.PythonScripts.standard import url_quote_plus
 from Products.ZCTextIndex.ParseTree import ParseError
+from lmu.policy.base.interfaces import ILMUSettings
 from plone import api
 from plone.app.contentlisting.interfaces import IContentListing
+from plone.app.layout.viewlets import common
 from plone.app.search.browser import Search as BaseSearch
 from plone.app.search.browser import quote_chars
+from plone.registry.interfaces import IRegistry
 from zope.component import getMultiAdapter
+from zope.component import getUtility
 
 
 class Search(BaseSearch):
@@ -185,3 +189,25 @@ class UserInfo(BrowserView):
         #self.portrait = OFSImage.Image(id=user.getUserName(), file=small_portrait, title='')
         self.portrait = portrait
         return self.template()
+
+
+class PathBarViewlet(common.PathBarViewlet):
+    """Replace the "Home" breadcrumb with the overrides from the control
+       panel."""
+    render = ViewPageTemplateFile('templates/path_bar.pt')
+
+    def update(self):
+        super(PathBarViewlet, self).update()
+
+        registry = getUtility(IRegistry)
+        self.lmu_settings = registry.forInterface(ILMUSettings)
+
+        portal_state = api.content.get_view(
+            name='plone_portal_state',
+            context=self.context,
+            request=self.request)
+        self.current_language = portal_state.locale().getLocaleID()
+
+        self.is_any_override_active = (
+            self.lmu_settings.show_breadcrumb_1 or
+            self.lmu_settings.show_breadcrumb_2)
