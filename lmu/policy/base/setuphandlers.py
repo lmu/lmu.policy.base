a050bc9efcebd72bce813469ed440c1708f34bbd
Manuel Reinhardt <reinhardt@syslab.com>
2015-09-29 13:00:37 +0100

    AutoUserMaker settings, refs #5

--- lmu/policy/base/setuphandlers.py
+++ lmu/policy/base/setuphandlers.py
@@ -12,6 +12,7 @@ def setupVarious(context):
         return
 
     _setupAutoRoleHeader(context)
+    _setupAutoUserMaker(context)
 
 
 def _setupAutoRoleHeader(context):
@@ -22,4 +23,18 @@ def _setupAutoRoleHeader(context):
     auto_role = acl_users['auto_role_header']
     auto_role.manage_changeProperties(
         match_roles=('Groupmembership; cms-admins-insp; Site Administrator',)
+    )
+
+
+def _setupAutoUserMaker(context):
+    acl_users = api.portal.get_tool('acl_users')
+    if 'AutoUserMakerPASPlugin' not in acl_users.objectIds():
+        log.warn('AutoUserMakerPASPlugin not found, please install'
+                 ' AutoUserMakerPASPlugin!')
+    auto_user = acl_users['AutoUserMakerPASPlugin']
+    auto_user.manage_changeProperties(
+        http_remote_user='HTTP_EDUPersonPrincipalName',
+        http_commonname='HTTP_DISPLAYNAME',
+        http_email='HTTP_MAIL',
+        auto_update_user_properties=1,
     )
