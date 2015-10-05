a16a69942057e2b3b7c21825e4cc188d39a3d173
Alexander Pilz <pilz@syslab.com>
2015-10-05 21:42:58 +0200

    move common settings to base

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='lmu.policy.base',
       version=version,
       description="A policy for lmu portals",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
@@ -25,24 +25,50 @@ setup(name='lmu.policy.base',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
-          #'Products.ShibbolethPermissions',
-          'Products.AutoRoleFromHostHeader',
-          'z3c.jbot',
-          'collective.solr',
+          # 'Products.ShibbolethPermissions',
+          'lmu.contenttypes.blog',
+          'lmu.contenttypes.pinnwand',
+          'lmu.contenttypes.polls',
+          'collective.deletepermission',
           'collective.indexing',
           'collective.quickupload',
+          'collective.solr',
+          'pas.plugins.shibboleth_headers',
+          'Pillow',
+          'Plone',
           'plone.api',
           'plone.app.changeownership',
+          'plone.app.contenttypes',
           'plone.app.layout',
           'plone.app.registry',
+          'plone.directives.form',
           'plone.z3cform',
+          'Products.AutoRoleFromHostHeader',
+          'Products.LongRequestLogger',
+          'wildcard.foldercontents',
           'z3c.form',
+          'z3c.jbot',
           'zope.app.schema',
           'zope.interface',
           'zope.schema',
           # -*- Extra requirements for Plone 4 -*-
-          'wildcard.foldercontents',
       ],
+      extras_require={
+          'test': [
+              'mock',
+              'plone.app.testing',
+              'plone.app.robotframework[debug]',
+              'fake-factory',
+          ],
+          'develop': [
+              'coverage',
+              'flake8',
+              'i18ndude',
+              'Sphinx',
+              'zptlint',
+          ],
+          'release': []
+      },
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
