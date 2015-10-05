d191c743055b143f57fbea50712000d0bfe32bbb
Manuel Reinhardt <reinhardt@syslab.com>
2015-10-05 16:19:54 +0100

    context_path_string indexer, refs #12

--- lmu/policy/base/indexers.py
+++ lmu/policy/base/indexers.py
@@ -1,5 +1,6 @@
 from Products.CMFCore.interfaces import IContentish
 from lmu.policy.base.controlpanel import ILMUSettings
+from plone.app.layout.navigation.root import getNavigationRoot
 from plone.indexer import indexer
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
@@ -17,3 +18,10 @@ def cms_system(obj):
     registry = getUtility(IRegistry)
     lmu_settings = registry.forInterface(ILMUSettings)
     return lmu_settings.cms_system
+
+
+@indexer(IContentish)
+def context_path_string(obj):
+    root_path = getNavigationRoot(obj)
+    context_path = '/'.join(obj.getPhysicalPath())[len(root_path):]
+    return context_path
